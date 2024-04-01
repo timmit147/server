615b9c10cf5b6e1957bc8a0abb27424032236002
Tim Meeuwsen <tim@doubleweb.nl>
2024-04-01 20:23:59 +0200

    works with ngrok exsternal server but after 2hour it stops

--- fetch/calc.js
+++ fetch/calc.js
@@ -1,4 +1,4 @@
-fetch('http://127.0.0.1:3000/calc', {
+fetch('https://084d-2001-1c02-1c19-7b00-c9d0-c468-4ce0-53fe.ngrok-free.app/calc', {
   method: 'POST', // Change the method to POST if you want to send data in the request body
   body: JSON.stringify({ data: 10 }), // Send the number 10 as JSON data in the request body
   headers: {
